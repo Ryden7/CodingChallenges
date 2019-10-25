e6346c788f34b11b995637be3c19e1a73865197a
Ryden7 <Rizwan.M.Mohammed1@gmail.com>
2019-10-25 15:14:56 -0600

    Contains duplicate

---
+++ Duplicate/Duplicate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6] { 6, 7, 1, 23, 6, 59};
+            Console.Write(ContainsDuplicate(numbers));
+            Console.Read();
+        }
+
+
+    public static bool ContainsDuplicate(int[] nums)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                    for (int j = 0; j < nums.Length; j++)
+                        if (nums[i] == nums[j])
+                            return true;
+
+                return false;
+            }
+    }
+}
