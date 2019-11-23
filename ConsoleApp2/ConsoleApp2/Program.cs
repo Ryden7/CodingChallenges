b8ae59ffa82cf402e624785ef1b71414b1144104
Ryden7 <Rizwan.M.Mohammed1@gmail.com>
2019-11-23 09:50:18 -0700

    String reverse

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "these are the words in a string you are going to reverse";
+            IEnumerable<char> p = s.Reverse();
+
+            foreach (var i in p)
+                Console.Write(i);
+
+            Console.WriteLine();
+            for (int i = s.Length; i > 0; i--)
+            {
+                Console.Write(s[i-1]);
+            }
+            Console.Read();
+
+        }
+    }
+}
