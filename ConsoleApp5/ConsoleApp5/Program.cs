4df54b98c463744cde09b6c150da11ebacd3ed36
Ryden7 <Rizwan.M.Mohammed1@gmail.com>
2019-12-21 20:35:29 -0700

    Arrays left rotation coding challenge

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[15] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97};
+            int[] retArr = rotLeft(input, 13);
+
+            foreach (var item in retArr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+
+        }
+
+        // Complete the rotLeft function below.
+        static int[] rotLeft(int[] a, int d)
+        {
+            int[] rotatedArray = new int[a.Length];
+
+            List<int> list = a.ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (d + 1 <= a.Length)
+                {
+                    rotatedArray[i] = list[d];
+                    d++;
+
+                }
+                else
+                {
+                    d = a.Length - d;
+                    rotatedArray[i] = list[d];
+                    d++;
+
+                }
+
+            }
+
+            return rotatedArray;
+        }
+    }
+}
